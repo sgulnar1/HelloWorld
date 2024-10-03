cfdbe39d1b8b3091fb2145d264e8a9b189478239
Gulnar H. Salehova <GHSalehova@bankofbaku.com>
2024-10-03 21:55:35 +0400

    first commit

---
+++ src/___$$$$$2zxc________xdcvf52TYUIOP$$$$$$$$$11111111111111.java
@@ -0,0 +1,5 @@
+public class ___$$$$$2zxc________xdcvf52TYUIOP$$$$$$$$$11111111111111 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/hello_world.java
@@ -0,0 +1,21 @@
+public class hello_world {
+    //WORA OOP
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        {
+            {
+
+            }
+        }
+    }
+
+    private String setName() {
+        System.out.println("This method use for set name");
+        return "Gulnar";
+    }
+    public String setName2() {
+        System.out.println("This method use for set name");
+        return "Gulnar";
+    }
+}
+
